77f8d875d2a636ecb1f1b817d6f303e2fcc87bce
Dan Lofquist <dan.lofquist@input-consulting.se>
2014-08-27 09:57:19 +0100

    Added more tests on articles

--- app/Input.Site.Tests/ArticleTests/ValidateAllArticles.cs
+++ app/Input.Site.Tests/ArticleTests/ValidateAllArticles.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 using Input.Site.Tests.Helpers;
 using InputSite.Interfaces;
@@ -19,14 +21,76 @@ namespace Input.Site.Tests.ArticleTests
         }
 
         [Test, TestCaseSource(typeof(ValidateAllArticles), "TestCases")]
-        public void should_parse(string article)
+        public void should_parse_markdown(string article)
+        {
+            Action parseAction = () =>
+            {
+                using (var sr = new StreamReader(article))
+                {
+                    var markdown = sr.ReadToEnd();
+                    var resource = _articleLocator.PathToResource(article);
+                    new ArticleParser(markdown, resource);
+                }
+            };
+
+            parseAction.ShouldNotThrow();
+        }
+
+        [Test, TestCaseSource(typeof(ValidateAllArticles), "TestCases")]
+        public void should_have_matching_sections(string article)
+        {
+            using (var sr = new StreamReader(article))
+            {
+                var articleSource = sr.ReadToEnd();
+                var beginSection = new Regex(Regex.Escape("@Section")).Matches(articleSource).Count;
+                var endSection = new Regex(Regex.Escape("@EndSection")).Matches(articleSource).Count;
+
+                beginSection.ShouldBeEquivalentTo(endSection);
+            }
+        }
+
+        [Test, TestCaseSource(typeof(ValidateAllArticles), "TestCases")]
+        public void should_have_title(string article)
         {
             using (var sr = new StreamReader(article))
             {
                 var markdown = sr.ReadToEnd();
-                var resource = _articleLocator.PathToResource(article);
-                var parsedArticle = new ArticleParser(markdown, resource);
+                if (markdown.Contains("@Tags"))
+                {
+                    var resource = _articleLocator.PathToResource(article);
+                    var parsedArticle = new ArticleParser(markdown, resource);
+                    parsedArticle.Title.Should().NotBeNullOrEmpty();
+                }
             }
         }
+
+        [Test, TestCaseSource(typeof(ValidateAllArticles), "TestCases")]
+        public void should_have_date(string article)
+        {
+            using (var sr = new StreamReader(article))
+            {
+                var markdown = sr.ReadToEnd();
+                if (markdown.Contains("@Tags"))
+                {
+                    var resource = _articleLocator.PathToResource(article);
+                    var parsedArticle = new ArticleParser(markdown, resource);
+                    parsedArticle.BlogDate.Should().NotBe(DateTime.MinValue);                   
+                }
+            }
+        }
+
+        [Test, TestCaseSource(typeof(ValidateAllArticles), "TestCases")]
+        public void should_have_matching_tags_if_present(string article)
+        {
+            using (var sr = new StreamReader(article))
+            {
+                var articleSource = sr.ReadToEnd();
+                var beginTags = new Regex(Regex.Escape("@Tags")).Matches(articleSource).Count;
+                var endTags = new Regex(Regex.Escape("@EndTags")).Matches(articleSource).Count;
+
+                beginTags.ShouldBeEquivalentTo(endTags);
+            }
+        }
+
     }
 }
