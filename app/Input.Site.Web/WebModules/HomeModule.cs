b6bafcac6cca63a33c66138d068e95fdb2f38b52
dan.lofquist <dan.lofquist@input-consulting.se>
2015-09-19 15:47:28 +0100

    added news, linkedin and made some small adjustements here and there

--- app/Input.Site.Web/WebModules/HomeModule.cs
+++ app/Input.Site.Web/WebModules/HomeModule.cs
@@ -13,13 +13,14 @@ namespace InputSite.WebModules
                 PageModel.Meta.HomeArticle = articleReader.ArticleById("home");
                 PageModel.Meta.AboutArticle = articleReader.ArticleById("about");
                 PageModel.Meta.CultureArticle = articleReader.ArticleById("culture");
-			    PageModel.Meta.CultureArticles = articleReader.ArticlesByCategory("culture").Where( a => a.ResourceName.Count( r => r == '/' ) > 0);
+                PageModel.Meta.CultureArticles = articleReader.ArticlesByCategory("culture").Where( a => a.ResourceName.Count( r => r == '/' ) > 0);
                 PageModel.Meta.ServicesArticle = articleReader.ArticleById("services");
                 PageModel.Meta.ServicesArticles = articleReader.ArticlesByCategory("services").Where( a => a.ResourceName.Count( r => r == '/' ) > 0);
                 PageModel.Meta.SalesArticle = articleReader.ArticleById("sales");
                 PageModel.Meta.SalesArticles = articleReader.ArticlesByCategory("sales").Where(a => a.ResourceName.Count(r => r == '/') > 0);
                 PageModel.Meta.ContactArticle = articleReader.ArticleById("contact");
                 PageModel.Meta.CrewArticle = articleReader.ArticleById("crew");
+                PageModel.Meta.LatestNews = articleReader.ArticlesByCategory("nyheter").Take(2);
 
                 return Negotiate
                        .WithView("_layout/home")
---
+++ site/views/nyheter/2015/09/ny-hemsida.markdown
@@ -0,0 +1,16 @@
+@Master['_layout/news-page.html'] 
+
+@Meta
+Author : Dan Lofquist
+Title: Input har en ny hemsida
+Date: 2014-09-19
+Tags: Nyheter
+BgImage : /assets/images/bg1.jpg
+Image : /static/img/thumbs/input-6.jpg
+@EndMeta
+
+@Section['Content']
+
+df df df sdafds fsd fafd sdaf sdaf sda fsda fsda fsd 
+
+@EndSection
