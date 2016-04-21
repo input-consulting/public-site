75bdb13badaf485a4ae3cffceca3ba111d2da59c
JanNordberg <raketjan@gmail.com>
2016-04-21 15:03:18 +0200

    webjob fix

--- app/PollStackForBlogPosts/Functions.cs
+++ app/PollStackForBlogPosts/Functions.cs
@@ -10,12 +10,12 @@ namespace Input.Site.WebJob
         // This function will be triggered based on the schedule you have set for this WebJob
         // This function will enqueue a message on an Azure Queue called queue
         [NoAutomaticTrigger]
-        public async static Task ManualTrigger(TextWriter log, int value, [Queue("queue")] string message)
+        public static void ManualTrigger(TextWriter log, int value, [Queue("queue")] out string message)
         {
             log.WriteLine("Function is invoked with value={0}", value);
             message = value.ToString();
             log.WriteLine("Following message will be written on the Queue={0}", message);
-            await PollSlackCommitGit();
+            PollSlackCommitGit().Wait();
         }
 
         public async static Task PollSlackCommitGit()
