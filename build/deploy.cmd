9b97e1beabeab9ae5806a079e35a3c83244b8387
dan.lofquist <dan.lofquist@tui.se>
2017-08-07 11:22:55 +0100

    changed deploy.cnd

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -98,6 +98,12 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
 :: 2. Select node version
 call :SelectNodeVersion
 
@@ -119,12 +125,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
   call :ExecuteCmd !GULP_CMD! build
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
-)
-
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
