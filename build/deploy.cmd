02354491d198345e3c091eee947f75d039a3a49f
dan.lofquist <dan.lofquist@tui.se>
2017-08-07 10:53:59 +0100

    changed deploy.cmd

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -88,26 +88,35 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
 :: 2. Select node version
 call :SelectNodeVersion
 
 :: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+::  pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! config set progress=false
   call :ExecuteCmd !NPM_CMD! install --only=prod
   call :ExecuteCmd !NPM_CMD! install --only=dev
   call :ExecuteCmd !NPM_CMD! run deploy:prod
   IF !ERRORLEVEL! NEQ 0 goto error
-  popd
+::  popd
 )
 
+::IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+::  pushd "%DEPLOYMENT_TARGET%"
+::  call :ExecuteCmd !NPM_CMD! config set progress=false
+::  call :ExecuteCmd !NPM_CMD! install --only=prod
+::  call :ExecuteCmd !NPM_CMD! install --only=dev
+::  call :ExecuteCmd !NPM_CMD! run deploy:prod
+::  IF !ERRORLEVEL! NEQ 0 goto error
+::  popd
+::)
+
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
