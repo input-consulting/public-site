5596f3fce61290c313fb540188ccf6b8871bc673
dan.lofquist <dan.lofquist@tui.se>
2017-08-11 09:46:30 +0100

    fixed scrolling bug

--- site/src/js/bootstrap.js
+++ site/src/js/bootstrap.js
@@ -27,35 +27,9 @@ $(document).ready(function () {
     $('#nav .navbar-nav li>a').click(function () {
         var link = $(this).attr('href');
         var posi = $(link).offset().top;
+        posi = $('#nav').position().top > 0 ? posi - $('#nav').height() : posi;
         $('body,html').animate({
             scrollTop: posi
         }, 700);
-    });
-
-
-// No moving parts atm.
-//    var isMobile = isMobile(navigator.userAgent);
-//    var news = $(".news-item");
-//    
-//    if ( isMobile ) {
-//        for (ii = 0, len = news.length; ii < len; ii++) {
-//            news.eq(ii).css("visibility","visible");
-//        }
-//    } else {
-//        if (news.length > 0) {
-//            news.eq(0).addClass("wow fadeInLeft");
-//            if (news.length === 2) {
-//                news.eq(1).addClass("wow fadeInRight");
-//            }
-//        }
-//    }
-//    
-//    new WOW({
-//          boxClass:     'wow',      // default
-//          animateClass: 'animated', // default
-//          offset:       0,          // default
-//          mobile:       false,       // default
-//          live:         true        // default
-//    }).init();
-    
+    });    
 });
