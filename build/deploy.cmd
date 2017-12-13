26a0c256daeb55cebbc022cbab08e554c25d020c
dan.lofquist <dan.lofquist@tui.se>
2017-12-13 08:29:17 +0000

    just testing

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -120,15 +120,15 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 )
 
 :: 2. Select node version
-call :SelectNodeVersion
+:: call :SelectNodeVersion
 
 :: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
+:: IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+::  pushd "%DEPLOYMENT_TARGET%"
+::  call :ExecuteCmd !NPM_CMD! install
+::  IF !ERRORLEVEL! NEQ 0 goto error
+::  popd
+::)
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
